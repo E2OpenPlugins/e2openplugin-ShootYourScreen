d958c658cf9176f235ceae8ee280fabd9f3ec806
Betacentauri <betacentauri@arcor.de>
2020-03-26 18:42:48 +0100

    Add support for special grab binaries
    Some grab binaries only write picture data to stdout and
    don't support png and bmp formats.
    The plugin now always uses -s parameter to write output to stdout.
    It writes the returned data into a file.
    Default format was changed to jpg.
    Warning: Mostly grab on hisilicon boxes don't support bmp and png.
    Even when in the plugin bmp or png is selected the screenshot files still
    contain jpg data.

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -35,7 +35,7 @@ config.plugins.shootyourscreen = ConfigSubsection()
 config.plugins.shootyourscreen.enable = ConfigEnableDisable(default=True)
 config.plugins.shootyourscreen.switchhelp = ConfigYesNo(default=True)
 config.plugins.shootyourscreen.path = ConfigSelection(default = "/media/hdd", choices = [("/media/hdd"), ("/media/usb"), ("/media/hdd1"), ("/media/usb1"), ("/tmp", "/tmp")])
-config.plugins.shootyourscreen.pictureformat = ConfigSelection(default = "bmp", choices = [("bmp", "bmp"), ("-j", "jpg"), ("-p", "png")])
+config.plugins.shootyourscreen.pictureformat = ConfigSelection(default = "jpg", choices = [("", "bmp"), ("-j", "jpg"), ("-p", "png")])
 config.plugins.shootyourscreen.jpegquality = ConfigSelection(default = "100", choices = [("10"), ("20"), ("40"), ("60"), ("80"), ("100")])
 config.plugins.shootyourscreen.picturetype = ConfigSelection(default = "all", choices = [("all", "OSD + Video"), ("-v", "Video"), ("-o", "OSD")])
 config.plugins.shootyourscreen.picturesize = ConfigSelection(default = "default", choices = [("default", _("Skin resolution")), ("-r 480", "480"), ("-r 576", "576"), ("-r 720", "720"), ("-r 1280", "1280"), ("-r 1920", "1920")])
@@ -96,17 +96,16 @@ class getScreenshot:
 		if not config.plugins.shootyourscreen.picturesize.value == "default":
 			cmdoptionsize = " " + str(config.plugins.shootyourscreen.picturesize.value)
 			cmd += cmdoptionsize
-		if not config.plugins.shootyourscreen.pictureformat.value == "bmp":
-			cmdoptionformat = " " + str(config.plugins.shootyourscreen.pictureformat.value)
-			cmd += cmdoptionformat
-			if config.plugins.shootyourscreen.pictureformat.value == "-j":
-				cmdoptionquality = " " + str(config.plugins.shootyourscreen.jpegquality.value)
-				cmd += cmdoptionquality
-		cmd += " %s" % filename
+		cmdoptionformat = " " + str(config.plugins.shootyourscreen.pictureformat.value)
+		cmd += cmdoptionformat
+		if config.plugins.shootyourscreen.pictureformat.getText() == "jpg":
+			cmdoptionquality = " " + str(config.plugins.shootyourscreen.jpegquality.value)
+			cmd += cmdoptionquality
+		cmd += " -s -q"
 		extra_args = (filename)
 		self.ScreenshotConsole.ePopen(cmd, self.gotScreenshot, extra_args)
 
-	def gotScreenshot(self, result, retval, extra_args = None):
+	def gotScreenshot(self, data, retval, extra_args = None):
 		if extra_args is not None:
 			filename = extra_args
 		else:
@@ -114,10 +113,19 @@ class getScreenshot:
 		
 		if not config.plugins.shootyourscreen.timeout.value == "off":
 			messagetimeout = int(config.plugins.shootyourscreen.timeout.value)
-			if retval == 0:
-				msg_text = _("Screenshot successfully saved as:\n%s") % filename
-				msg_type = MessageBox.TYPE_INFO
+			error = False
+			if retval == 0 and filename:
+				try:
+					with open(filename, "wb") as file:
+						file.write(data)
+					msg_text = _("Screenshot successfully saved as:\n%s") % filename
+					msg_type = MessageBox.TYPE_INFO
+				except Exception, e:
+					print "[ShootYourScreen] Error creating file", e
+					error = True
 			else:
+				error = True
+			if error:
 				msg_text = _("Grabbing Screenshot failed !!!")
 				msg_type = MessageBox.TYPE_ERROR
 			AddNotification(MessageBox, msg_text, MessageBox.TYPE_INFO, timeout = messagetimeout)
@@ -131,11 +139,11 @@ class getScreenshot:
 
 		screenshottime = "screenshot_" + now
 		
-		if config.plugins.shootyourscreen.pictureformat.value == "bmp":
+		if config.plugins.shootyourscreen.pictureformat.getText() == "bmp":
 			fileextension = ".bmp"
-		elif config.plugins.shootyourscreen.pictureformat.value == "-j":
+		elif config.plugins.shootyourscreen.pictureformat.getText() == "jpg":
 			fileextension = ".jpg"
-		elif config.plugins.shootyourscreen.pictureformat.value == "-p":
+		elif config.plugins.shootyourscreen.pictureformat.getText() == "png":
 			fileextension = ".png"
 
 		picturepath = getPicturePath()
@@ -188,7 +196,7 @@ class ShootYourScreenConfig(Screen, ConfigListScreen):
 		if config.plugins.shootyourscreen.enable.value == True:
 			self.list.append(getConfigListEntry(_("Screenshot of :"), config.plugins.shootyourscreen.picturetype))
 			self.list.append(getConfigListEntry(_("Format for screenshots :"), config.plugins.shootyourscreen.pictureformat))
-			if config.plugins.shootyourscreen.pictureformat.value == "-j":
+			if config.plugins.shootyourscreen.pictureformat.getText() == "jpg":
 				self.list.append(getConfigListEntry(_("Quality of jpg picture :"), config.plugins.shootyourscreen.jpegquality))
 			self.list.append(getConfigListEntry(_("Picture size (width) :"), config.plugins.shootyourscreen.picturesize))
 			self.list.append(getConfigListEntry(_("Path for screenshots :"), config.plugins.shootyourscreen.path))
@@ -236,7 +244,7 @@ class ShootYourScreenConfig(Screen, ConfigListScreen):
 			config.plugins.shootyourscreen.enable.setValue(1)
 			config.plugins.shootyourscreen.switchhelp.setValue(1)
 			config.plugins.shootyourscreen.path.setValue("/media/hdd")
-			config.plugins.shootyourscreen.pictureformat.setValue("bmp")
+			config.plugins.shootyourscreen.pictureformat.setValue("-j")
 			config.plugins.shootyourscreen.jpegquality.setValue("100")
 			config.plugins.shootyourscreen.picturetype.setValue("all")
 			config.plugins.shootyourscreen.picturesize.setValue("default")
